2207c07f4ccfd85939376f7f3df23a505faa92c3
Ermakovis <ermakov.ugra@gmail.com>
2022-12-27 15:19:49 +0500

    homework c# finished

---
+++ Task066/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+int GetNumber(string message)
+{
+    int result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Неверная размерность");
+        }
+    }
+
+    return result;
+}
+
+int SumFromMtoN (int m, int n)
+{
+    if (m == n) return n;
+    else return m + SumFromMtoN(m+1, n);
+}
+
+int m = GetNumber("Enter the M number");
+int n = GetNumber("Enter the N number");
+
+Console.WriteLine($"{SumFromMtoN(m, n)}");
---
+++ Task068/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+int GetNumber(string message)
+{
+    int result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Неверная размерность");
+        }
+    }
+
+    return result;
+}
+
+int AkkermanFunction (int m, int n)
+{
+    if (m == 0) return n+1;
+    else if ((m > 0) && (n == 0)) return AkkermanFunction(m-1, 1);
+    else if ((m > 0) && (n > 0))  return AkkermanFunction(m-1, AkkermanFunction(m, n-1));
+    else return 0;
+}
+
+int m = GetNumber("Enter the M number");
+int n = GetNumber("Enter the N number");
+
+Console.WriteLine($"{AkkermanFunction(m, n)}");
